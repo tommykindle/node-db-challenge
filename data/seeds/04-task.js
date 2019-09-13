88376d3fd6bffcab14d2348b7465e244ba56bab2
Tommy <tommykindle83@gmail.com>
2019-09-13 10:29:45 -0700

    completed all seeds ready to make models

--- data/seeds/04-task.js
+++ data/seeds/04-task.js
@@ -1,13 +1,20 @@
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
+exports.seed = function (knex) {
+
+
+  return knex('table_name').truncate()
     .then(function () {
-      // Inserts seed entries
+
       return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        { description: 'Put gas in the lawnmower', notes: 'some notes', completed: true, project_id: 1 },
+        { description: 'Start the lawnmower mow the yard', notes: 'do not miss a spot', completed: true, project_id: 1 },
+        { description: 'Sharpen pruning sheers ', notes: 'do not cut yourself', completed: false, project_id: 2 },
+        { description: 'Prun tree and clean up ', notes: 'put all limbs and leaves in trash', completed: false, project_id: 2 },
+        { description: 'Put gas in the weedeater', notes: 'some notes here', completed: true, project_id: 3 },
+        { description: 'Start weedeater weed eat around entire yard', notes: 'some notes here', completed: true, project_id: 3 },
+        { description: 'Get the rake from the shed ', notes: 'some notes here', completed: true, project_id: 4 },
+        { description: 'Rake all loose grass and weeds from the yard put in piles', notes: 'some notes here', completed: true, project_id: 4 }
+
       ]);
     });
 };
